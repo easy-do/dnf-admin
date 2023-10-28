398f83266d3444e22da0ce913f0c72e42109d1ec
于占峰 <yuzhanfeng@yunlizhihui.com>
2023-10-28 14:30:14 +0800

    拓展脚本逻辑

--- be/src/main/java/plus/easydo/dnf/handler/PingDpReportHandler.java
+++ be/src/main/java/plus/easydo/dnf/handler/PingDpReportHandler.java
@@ -2,15 +2,13 @@ package plus.easydo.dnf.handler;
 
 import cn.hutool.core.date.DatePattern;
 import cn.hutool.core.date.LocalDateTimeUtil;
-import cn.hutool.json.JSONUtil;
 import org.springframework.stereotype.Service;
 import plus.easydo.dnf.constant.ReportTypeConstant;
-import plus.easydo.dnf.dto.UerLoginOutDto;
 import plus.easydo.dnf.manager.CacheManager;
 import plus.easydo.dnf.vo.DpResult;
 
-import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * @author laoyu
@@ -22,7 +20,13 @@ import java.util.Collections;
 public class PingDpReportHandler implements DpReportHandler {
     @Override
     public DpResult handler(String type, String value) {
-        CacheManager.DP_PING_CACHE.set(0, LocalDateTimeUtil.format(LocalDateTimeUtil.now(), DatePattern.NORM_DATETIME_MINUTE_FORMATTER));
+        List<String> pingCache = CacheManager.DP_PING_CACHE;
+        String pingTime = LocalDateTimeUtil.format(LocalDateTimeUtil.now(), DatePattern.NORM_DATETIME_MINUTE_FORMATTER);
+        if (pingCache.isEmpty()) {
+            CacheManager.DP_PING_CACHE.add(0, pingTime);
+        } else {
+            CacheManager.DP_PING_CACHE.set(0, pingTime);
+        }
         return DpResult.build(ReportTypeConstant.PING, Collections.emptyList());
     }
 }
